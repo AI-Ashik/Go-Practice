dcea18c107d36235e2e0ae4ca71269079ff28d69
AI-Ashik <arifulislamashik07@gmail.com>
2025-11-15 20:34:30 +0600

    go practice

---
+++ First Program/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	a := 2
+
+	switch a {
+	case 1:
+		fmt.Println("a is 1")
+	case 2:
+		fmt.Println("a is 2")
+	case 3:
+		fmt.Println("a is 3")
+	default:
+		fmt.Println("a is something else")
+		
+	}
+}
