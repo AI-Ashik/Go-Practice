cd4b4de34f41335b3f04c0b8973924f7231b8a56
AI-Ashik <arifulislamashik07@gmail.com>
2025-11-22 23:17:04 +0600

    created a small summation app using user inputs and functions

--- function-practice/function.go
+++ function-practice/function.go
@@ -2,11 +2,52 @@ package main
 
 import "fmt"
 
-func main() {
+func welcomeMessage() {
 
+	fmt.Println("Welcome to the application")
+}
+
+func getName() string {
 	var name string
 	fmt.Println("Enter your name : ")
 	fmt.Scanln(&name)
-	fmt.Println("Your name is : ", name)
+	fmt.Println("Welcome : ", name)
+	return name
+}
+
+func getNumbers() (int, int) {
+	var num1 int
+	var num2 int
+	fmt.Println("Enter numbe 1 : ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter numbe 2 : ")
+	fmt.Scanln(&num2)
+	return num1, num2
+}
+
+func summation(num1 int, num2 int) int {
+
+	sum := num1 + num2
+	return sum
+}
+
+func totalSum(sum int) {
+
+	fmt.Println("Sum of two numbers is : ", sum)
+
+}
+
+func finalMessage(name string) {
+	fmt.Println("Thank you for using the appliocation", name)
+
+}
+
+func main() {
+	welcomeMessage()
+	name := getName()
+	num1, num2 := getNumbers()
+	sum := summation(num1, num2)
+	totalSum(sum)
+	finalMessage(name)
 
 }
